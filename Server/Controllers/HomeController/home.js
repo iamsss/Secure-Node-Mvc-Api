4de0b26e04964716d00ee28bf643b4655579147f
iamsss <saurav9760@gmail.com>
2018-06-07 05:50:16 +0530

    Passing Data From Controller to View

--- Server/Controllers/HomeController/home.js
+++ Server/Controllers/HomeController/home.js
@@ -3,7 +3,7 @@ const {app} = require('../../server');
 app.get('/', (req, res) => res.render(
     'home',{
         
-        pageTitle: 'About Page',
+        pageTitle: 'Home Page',
         greeting: 'Welcome to My Home Page'
     }
 ));
